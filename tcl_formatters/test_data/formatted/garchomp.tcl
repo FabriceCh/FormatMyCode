797cb82f9d261e7f653bed4e9de79f38d8757572
alizeegagnon <alizeegagnon602@hotmail.com>
2022-10-31 13:15:49 -0400

    added realistic test data

---
+++ tcl_formatters/test_data/formatted/garchomp.tcl
@@ -0,0 +1,22 @@
+gigachomp::garchomp                       \
+    -species {garchomp::garchopm::asdasd} \
+    -color {blue::brown::red}             \
+    -cry {hhhhrrraarrrgh}                 \
+    -attacks { 
+        attack::dig                  \
+            -type ground             \
+            -kind physical           \
+            -power 100               \
+            -accuracy 100            \
+            -pp $pp_left             \
+            -effective_against [list \
+                electric             \
+                poison               \
+            ]
+    } \
+    -caracteristics {
+        set level 99
+        set ability sandshit
+        set nature adamant
+    }       
+
