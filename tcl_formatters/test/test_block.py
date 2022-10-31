a8ec82d6279b9d6c747f6253a23158c5ab81affb
fab <fab@ubu.myguest.virtualbox.org>
2022-10-31 11:32:57 -0400

    added a test

--- tcl_formatters/test/test_block.py
+++ tcl_formatters/test/test_block.py
@@ -2,6 +2,10 @@ from tcl_formatters.source.block import Block
 from tcl_formatters.test.conftest import raw_block
 
 
+def test_block_creation(raw_block):
+    block = Block(raw_block)
+    assert len(block.lines) == 5
+
 def test_block_get_longest_line_length(raw_block):
     block = Block(raw_block)
     assert block._get_longest_line_length() == 22
