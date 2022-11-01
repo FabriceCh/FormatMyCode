b66eb29e620f7407edbf7463d373c19add85da55
fab <fab@ubu.myguest.virtualbox.org>
2022-11-01 11:06:23 -0400

    fixed failing block tests

--- tcl_formatters/test/test_block.py
+++ tcl_formatters/test/test_block.py
@@ -6,9 +6,13 @@ def test_block_creation(unformatted_block):
     block = Block(unformatted_block)
     assert len(block.lines) == 4
 
+def test_block_get_longest_line_length_single(unformatted_block):
+    block = Block(["\t\tcode \\\n"])
+    assert block._get_longest_line_length() == 15
+
 def test_block_get_longest_line_length(unformatted_block):
     block = Block(unformatted_block)
-    assert block._get_longest_line_length() == 22
+    assert block._get_longest_line_length() == 32
 
 def test_block_format(unformatted_block, formatted_block):
     block = Block(unformatted_block)
