8329159eac99006de1757aec1f25eab6a61ddc02
fab <fab@ubu.myguest.virtualbox.org>
2022-11-05 17:56:18 -0400

    range param and parse function tested

--- src/format_my_code.py
+++ src/format_my_code.py
@@ -4,7 +4,8 @@ import click
 
 @click.command
 @click.option("-f", "--filename")
-def format_my_code(filename: str):
+@click.option("-r", "--range")
+def format_my_code(filename: str, range: str = ""):
     lines = read_lines(filename)
     new_lines = EolSlashFormatter.format_block(lines)
     write_lines(filename, new_lines)
@@ -20,6 +21,17 @@ def write_lines(filename: str, lines: List[str]) -> None:
     file.writelines(lines)
     file.close()
 
+def parse_user_specified_ranges(range: str) -> List[List[int]]:
+    """Parses user specified range to generate a usable list of ranges
 
+    Args:
+        range (str): user specified range(s) e.g. "5-10", "20-30,40-50"
+
+    Returns:
+        List[List[int]]: usable list of ranges e.g. [[5, 10]], [[20, 30], [40, 50]]
+    """
+
+    return [[int(single_range.split("-")[0]), int(single_range.split("-")[1])] for single_range in range.split(",")]
+    
 if __name__ == "__main__":
     format_my_code()
